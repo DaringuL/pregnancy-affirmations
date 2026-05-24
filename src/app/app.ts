2ab3cc096833f5bb10fe5e787ca95b413a2f6ec9
Daringu <love.dzh.r@gmail.com>
2026-05-24 20:25:55 +0200

    First functionallity

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, signal } from '@angular/core';
+import {AffirmationCardComponent} from './pregnancy-affirmations-card/pregnancy-affirmations-card';
+import {GradientPicker} from './gradient-picker/gradient-picker';
+import gradients from './gradient-picker/gradients';
+
+const STORAGE_KEY = 'selected_gradient';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AffirmationCardComponent, GradientPicker],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pregnancy-affirmations');
+  protected gradientValue = signal(localStorage.getItem(STORAGE_KEY) || gradients[0].value);
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem(STORAGE_KEY, this.gradientValue());
+    });
+  }
 }
---
+++ src/app/gradient-picker/gradients.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    "name": 'Light teal',
+    "value": 'linear-gradient(135deg, #fdfbf7 0%, #eef5f3 100%)'
+  },
+  {
+    "name": 'Pink storm',
+    "value": 'linear-gradient(-45deg, #250e2c, #837ab6, #9d85b6, #cc84b3, #f6a5c0, #f7c2ca)'
+  },
+  {
+    "name": 'Lavender Pastel',
+    "value": 'linear-gradient(0, #A5C0EE, #FBC5EC)'
+  },
+  {
+    "name": 'Minty Ocean',
+    "value": 'linear-gradient(0, #74EBD5, #9FACE6)'
+  },
+]
